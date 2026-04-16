1f190ba9bd967e8a278a27a33798073b3f95fd54
João Marcelo <joaomarcelo071012@gmail.com>
2026-04-16 13:40:46 -0300

    Delete MVC e LocalStorage - PRova N1 directory

--- MVC e LocalStorage - PRova N1/src/model/Tarefa.mjs
+++
@@ -1,8 +0,0 @@
-// Modelo da entidade Tarefa
-export class Tarefa {
-    constructor(descricao) {
-        this.id = Date.now().toString(); // ID único gerado com timestamp
-        this.descricao = descricao;
-        this.concluida = false;
-    }
-}
